a21df3567bb31b04b8c3aed961c02b2257d0b06f
施 彬 <shibin@beeoz.com>
2018-02-24 16:25:35 +0800

    Spring Boot 使用Swagger2构建RESTful API http://www.spring4all.com/article/251

--- src/main/java/com/example/demo/test3/UserController.java
+++ src/main/java/com/example/demo/test3/UserController.java
@@ -1,5 +1,8 @@
 package com.example.demo.test3;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -16,6 +19,7 @@ public class UserController {
      */
     static Map<Long,User> users = Collections.synchronizedMap(new HashMap<>());
 
+    @ApiOperation(value="获取用户列表", notes="")
     @GetMapping(value = "/")
     public List<User> getUserList(){
 
@@ -25,6 +29,8 @@ public class UserController {
         return r;
     }
 
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @PostMapping(value = "/")
     public String postUser(@ModelAttribute User user) {
         // 处理"/users/"的POST请求，用来创建User
@@ -33,6 +39,9 @@ public class UserController {
         return "success";
     }
 
+
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @GetMapping("/{id}")
     public User getUser(@PathVariable int id){
         // 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
@@ -40,6 +49,11 @@ public class UserController {
         return users.get(id);
     }
 
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
     @PutMapping("/{id}")
     public String putUser(@PathVariable Long id, @ModelAttribute User user) {
         // 处理"/users/{id}"的PUT请求，用来更新User信息
@@ -50,6 +64,8 @@ public class UserController {
         return "success";
     }
 
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @DeleteMapping(value = "/{id}")
     public String deleteUser(@PathVariable Long id) {
         // 处理"/users/{id}"的DELETE请求，用来删除User
